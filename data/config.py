5c232ea7bf5f2eac744eb32be062b0be4c1e7967
tohir <tohirerkinov181@gmail.com>
2023-12-20 12:17:29 +0000

    first commit

---
+++ data/config.py
@@ -0,0 +1,10 @@
+from environs import Env
+
+# environs kutubxonasidan foydalanish
+env = Env()
+env.read_env()
+
+# .env fayl ichidan quyidagilarni o'qiymiz
+BOT_TOKEN = '6685781373:AAG24z2XI80xQTM-nFp8fkJAEvsRv64oCV8'  # Bot toekn
+ADMINS = [5689522270,]  # adminlar ro'yxati
+
---
+++ handlers/users/start.py
@@ -0,0 +1,17 @@
+from aiogram import types
+from aiogram.dispatcher.filters.builtin import CommandStart
+from keyboards.default.start_kb import start_keyboards
+
+from loader import dp
+
+
+@dp.message_handler(CommandStart())
+async def bot_start(message: types.Message):
+    await message.answer(f"Salom, {message.from_user.full_name}!"
+                         f"Menyuni tanlang", reply_markup=start_keyboards)
+
+
+@dp.message_handler(text="📷 Remove BG")
+async def bot_start(message: types.Message):
+    await message.answer(f"Orqa fonini o'chirish uchun rasm yuboring!", reply_markup=types.ReplyKeyboardRemove())
+`
